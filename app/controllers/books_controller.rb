a17e5647b18aa35939d54736c4b5c23ca9dd0ed6
松永実華 <mika@matsunagamikanoMacBook-Pro.local>
2020-08-22 21:57:34 +0900

    [Finish]DMM WEB CAMP 課題2 アプリケーション作成作成:基礎

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,14 @@ class BooksController < ApplicationController
 
   def create
    @book = Book.new(book_params)
-   @book.save
-   redirect_to book_path(@book.id)
+
+   if @book.save
+    flash[:notice] = 'Book was successfully cyeated'
+    redirect_to book_path(@book.id) # , notice: 'Book was successfully cyeated'
+   else
+    @books = Book.all
+    render :index
+   end
   end
 
   def edit
@@ -22,9 +28,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:successfully] = "Book was successfully updated"
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :edit
+    end
   end
 
   def destroy
